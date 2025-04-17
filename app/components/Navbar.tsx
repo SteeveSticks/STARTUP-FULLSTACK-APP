1c5c0cb83ee7e7ebd0f2f3c41de498a6b402b7f6
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-04-17 20:35:58 +0100

    Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="px-5 py-4 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+
+        <div className="flex items-center gap-5"></div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
