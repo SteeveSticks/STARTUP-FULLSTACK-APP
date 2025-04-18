5b909603558bdfb0db23fb07c4a2f69b633cf5d3
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-04-18 01:20:05 +0100

    configuration of assets folder

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [animate, typography],
+};
+
+export default config;
