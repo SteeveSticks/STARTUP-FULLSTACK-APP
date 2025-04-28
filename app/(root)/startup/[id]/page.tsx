7cecdd139041eda2520a2c8a60ec5ad578b302b7
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-04-28 10:32:09 +0100

    single startupPage

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,10 +1,25 @@
+import { formatDate } from "@/lib/utils";
+import { client } from "@/sanity/lib/client";
+import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
 import React from "react";
+
+export const experimental_ppr = true;
 
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
+
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  if (!post) return notFound();
+
   return (
     <>
-      <h1 className="text-3xl">This is a startup number: {id}</h1>
+      <section className="pink_container !min-h-[230px] pattern">
+        <p className="tag">{formatDate(post?._createdAt)}</p>
+
+        <h1 className="heading">{post.title}</h1>
+      </section>
     </>
   );
 };
