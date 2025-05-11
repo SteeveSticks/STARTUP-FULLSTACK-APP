0dce34a525607ed5eb6b6daa40eb533cd3531f6a
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-05-11 01:12:15 +0100

    user profile 0.1

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,14 +1,14 @@
 import { auth } from "@/auth";
-import UserStartups from "@/components/UserStartups";
 import { client } from "@/sanity/lib/client";
 import { AUTHOR_BY_ID_QUERY } from "@/sanity/lib/queries";
-import Image from "next/image";
 import { notFound } from "next/navigation";
-import React, { Suspense } from "react";
+import Image from "next/image";
+import UserStartups from "@/components/UserStartups";
+import { Suspense } from "react";
 
 export const experimental_ppr = true;
 
-const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
   const session = await auth();
 
@@ -24,6 +24,7 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
               {user.name}
             </h3>
           </div>
+
           <Image
             src={user.image}
             alt={user.name}
@@ -35,14 +36,13 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
           <p className="text-30-extrabold mt-7 text-center">
             @{user?.username}
           </p>
-          <p className="text-center mt-1 text-14-normal">{user?.bio}</p>
+          <p className="mt-1 text-center text-14-normal">{user?.bio}</p>
         </div>
 
-        <div className="flex-1 flex-col gap-5 lg:-mt-5">
+        <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
           <p className="text-30-bold">
             {session?.id === id ? "Your" : "All"} Startups
           </p>
-
           <ul className="card_grid-sm">
             <Suspense fallback={<p>Loading...</p>}>
               <UserStartups id={id} />
@@ -54,4 +54,4 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   );
 };
 
-export default page;
+export default Page;
